10ee3be28c694a7ca55b7acfa4b7b614ca361388
Sergey_js15 <serj.777.front@gmail.com>
2023-11-10 23:11:26 +0200

    modified code

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import { useState } from 'react'
 import './App.css'
-import { buttonStyle } from './const/index'
-import { Button } from './Button/Button'
 
-function App() {
-  const [colors, setColors] = useState('black')
-  const colorStyleFunc = () => {
-    setColors(colors === 'black' ? 'red' : 'black')
-  }
+const CustomButton = ({ backgroundColor, textColor, borderRadius, text }) => {
+  const buttonStyles = {
+    backgroundColor: backgroundColor || 'blue',
+    color: textColor || 'white',
+    borderRadius: borderRadius || '5px',
+    padding: '10px',
+    margin: '5px',
+    cursor: 'pointer',
+  };
+
   return (
-    <section>
-      <h1 style={{ color: colors }}>Title h1</h1>
-      < Button
-        text="This button + children"
-        color="green"
-        border="2px solid black"
-        width="200px"
-        height="200px"
-        fontSize="22px"
-        padding="20px"
-        colorStyleFunc={colorStyleFunc}
-        buttonStyle={buttonStyle}
-      >
-        <p>children 1</p>
-        <p>children 2</p>
-      </Button>
-    </section>
-  )
-}
+    <button style={buttonStyles}>
+      {text}
+    </button>
+  );
+};
 
-export default App
+const App = () => {
+  return (
+    <div>
+      <CustomButton backgroundColor="red" textColor="white" text="Button 1" />
+      <CustomButton backgroundColor="green" textColor="black" borderRadius="8px" text="Button 2" />
+      <CustomButton backgroundColor="yellow" textColor="red"  text="Button 3" />
+      <CustomButton text="Button 4" borderRadius={"20px"}/>
+    </div>
+  );
+};
+
+export default App;
--- src/const/index.js
+++
@@ -1,6 +0,0 @@
-export const buttonStyle = {
-  border: '2px solid red',
-  borderRadius: '10px',
-  margin: '5px auto',
-  padding: '5px 10px'
-}
